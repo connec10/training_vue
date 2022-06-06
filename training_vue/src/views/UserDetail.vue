c8f42836a6ee137aa2a68e9b1f675fdeef936eac
fukuda0910 <105188028+fukuda0910@users.noreply.github.com>
2022-06-06 17:03:27 +0900

    課題3-2の4.
    ＜追加＞
    ・ユーザー名とユーザー説明を表示
    ・getUserメソッド：
    　Api.jsからIDが一致するユーザのデータを取得する

--- training_vue/src/views/UserDetail.vue
+++ training_vue/src/views/UserDetail.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
     <div>ユーザー詳細</div>
+    <div>{{ user.name }}</div>
+    <div>{{ user.description }}</div>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+  import Api from '../../mock/Api.js'
+
+  export default {
+    data: function () {
+      return {
+        user: {},
+      }
+    },
+    created: function () {
+      this.getUser()
+    },
+    watch: { '$route': 'getUser' },
+    methods: {
+      getUser: function () {
+        var userId = this.$route.params.userId
+        Api.getUser(userId, function (p, user) {
+          this.user = user
+        }.bind(this))
+      },
+    },
+  }
 </script>
 
 <style>
