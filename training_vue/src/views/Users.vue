7e17b3f5321004d3b30bac54c2f833b15420e612
fukuda0910 <105188028+fukuda0910@users.noreply.github.com>
2022-06-06 15:08:19 +0900

    課題3-1
    ＜追加＞
    ・データ取得中に固定文字を表示する処理
    ・Api.jsのgetUsersを呼び出してユーザ一覧を表示する処理

--- training_vue/src/views/Users.vue
+++ training_vue/src/views/Users.vue
@@ -1,42 +1,47 @@
 <template>
   <div id="app">
     <div>ユーザー一覧</div>
-    <div>読み込み中…</div>
+    <div v-show="isUserDataLoading">読み込み中…</div>
     <table>
       <thead>
         <tr>
           <th>ID</th><th>名前</th><th>操作</th>
         </tr>
       </thead>
-      <!-- <tbody>
-        <tr v-for="user in users" v-bind:key="id">
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id">
           <td>{{ user.id }}</td><td>{{ user.name }}</td><td>詳細</td>
         </tr>
-      </tbody> -->
+      </tbody>
     </table>
   </div>
 </template>
 
 <script>
-import Api from '../../mock/Api.js'
-export default {
-  data: function () {
-    return {
-      users: Api.getUsers
-    }
-  },
-  created: function () {
-    this.fetchData()
-  },
-  watch: { '$route': 'fetchData' },
-  methods: {
-    fetchData: function () {
-      // APIを実行して、this.hogeにデータを設定する
-    }
+  import Api from '../../mock/Api.js'
+
+  export default {
+    data: function () {
+      return {
+        users: [],
+        isUserDataLoading: false,
+      }
+    },
+    created: function () {
+      this.getUsers()
+    },
+    watch: { '$route': 'getUsers' },
+    methods: {
+      getUsers: function () {
+        this.isUserDataLoading = true
+        Api.getUsers(function (err, users) {
+          this.users = users
+        }.bind(this))
+        this.isUserDataLoading = false
+      }
+    },
   }
-}
 </script>
 
 <style>
-
 </style>
