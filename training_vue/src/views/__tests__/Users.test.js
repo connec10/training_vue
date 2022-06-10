2fc3ecd8fe4ce805dcdcb00ebfe5c3dde7aa3c74
fukuda0910 <105188028+fukuda0910@users.noreply.github.com>
2022-06-10 15:44:59 +0900

    Update Users.test.js
    ユーザ一覧テーブルのテストを追加

--- training_vue/src/views/__tests__/Users.test.js
+++ training_vue/src/views/__tests__/Users.test.js
@@ -1,33 +1,55 @@
 import { mount } from '@vue/test-utils'
 import Users from '../Users.vue'
 
-describe('Users.vue: 読み込み中表示テスト', () => {
-  const wrapper = mount(Users)
+const wrapper = mount(Users)
+
+describe('Users.vue: 読み込み中表示', () => {
   const item = wrapper.findAll('div').at(1)
   const msg = '「読み込み中…」.isVisible(): '
 
-  test('', done => {
-    console.log('TEST:読み込み中のときは「読み込み中…」を表示する')
+  test('読み込み中のときは「読み込み中…」を表示する', done => {
     wrapper.setData({ isUserDataLoading: true })
     expect(wrapper.vm.isUserDataLoading).toBe(true)
 
     wrapper.vm.$nextTick(() => {
-      console.log(msg + JSON.stringify(item.isVisible()))
+      // console.log(msg + item.isVisible())
       expect(item.isVisible()).toBe(true)
       done()
     })
-  });
+  })
   
-  test('', done => {
-    console.log('TEST:読み込みをしていないときは「読み込み中…」を表示しない')
+  test('読み込みをしていないときは「読み込み中…」を表示しない', done => {
     wrapper.setData({ isUserDataLoading: false })
     expect(wrapper.vm.isUserDataLoading).toBe(false)
 
     wrapper.vm.$nextTick(() => {
-      console.log(msg + JSON.stringify(item.isVisible()))
+      // console.log(msg + item.isVisible())
       expect(item.isVisible()).toBe(false)
       done()
     })
-  });
+  })
+})
 
+describe('Users.vue: ユーザー一覧テーブル', () => {
+  const testUsers = [
+    {
+      id: 1,
+      name: "test",
+    },
+    {
+      id: 2,
+      name: "test2",
+    },
+  ]
+  
+  test('usersの数だけ行が表示される', done => {
+    wrapper.setData({ users: testUsers })
+
+    wrapper.vm.$nextTick(() => {
+      const item = wrapper.findAll('tbody>tr')
+      // console.log('item: ' + JSON.stringify(item.length))
+      expect(item.length).toBe(2)
+      done()
+    })
+  })
 })
