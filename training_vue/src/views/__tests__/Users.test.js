7638d8483c93285c71cdd9c422c8277de64040ae
fukuda0910 <105188028+fukuda0910@users.noreply.github.com>
2022-06-10 13:51:30 +0900

    Update Users.test.js
    重複部分の記述を変更

--- training_vue/src/views/__tests__/Users.test.js
+++ training_vue/src/views/__tests__/Users.test.js
@@ -1,39 +1,33 @@
 import { mount } from '@vue/test-utils'
 import Users from '../Users.vue'
 
-describe('Users.vue: 読み込み中のときは「読み込み中…」を表示する', () => {
+describe('Users.vue: 読み込み中表示テスト', () => {
+  const wrapper = mount(Users)
+  const item = wrapper.findAll('div').at(1)
+  const msg = '「読み込み中…」.isVisible(): '
 
   test('', done => {
-    const wrapper = mount(Users)
-    const item = wrapper.findAll('div').at(1)
-    console.log('item.isVisible():' + JSON.stringify(item.isVisible()));      
-
+    console.log('TEST:読み込み中のときは「読み込み中…」を表示する')
     wrapper.setData({ isUserDataLoading: true })
     expect(wrapper.vm.isUserDataLoading).toBe(true)
 
     wrapper.vm.$nextTick(() => {
-      console.log('item.isVisible():' + JSON.stringify(item.isVisible()));
-      
+      console.log(msg + JSON.stringify(item.isVisible()))
       expect(item.isVisible()).toBe(true)
       done()
     })
   });
-})
-
-describe('Users.vue: 読み込みをしていないときは「読み込み中…」を表示しない', () => {
-
+  
   test('', done => {
-    const wrapper = mount(Users)
-    const item = wrapper.findAll('div').at(1)
-    console.log('item.isVisible():' + JSON.stringify(item.isVisible()));      
-
+    console.log('TEST:読み込みをしていないときは「読み込み中…」を表示しない')
     wrapper.setData({ isUserDataLoading: false })
     expect(wrapper.vm.isUserDataLoading).toBe(false)
 
     wrapper.vm.$nextTick(() => {
-      console.log('item.isVisible():' + JSON.stringify(item.isVisible()));
+      console.log(msg + JSON.stringify(item.isVisible()))
       expect(item.isVisible()).toBe(false)
       done()
     })
   });
+
 })
