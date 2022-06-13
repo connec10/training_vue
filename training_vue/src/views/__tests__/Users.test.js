fd0c3ebe0f3567689bbcaba257731f50c837d661
fukuda0910 <105188028+fukuda0910@users.noreply.github.com>
2022-06-13 17:58:21 +0900

    Update Users.test.js

--- training_vue/src/views/__tests__/Users.test.js
+++ training_vue/src/views/__tests__/Users.test.js
@@ -1,31 +1,29 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
+import Api from 'training_vue/mock/Api.js'
 import Users from '../Users.vue'
 
-const wrapper = mount(Users)
+const wrapper = shallowMount(Users, {
+  stubs: ['router-link', 'router-view']
+})
 
 describe('Users.vue: 読み込み中表示', () => {
   const item = wrapper.findAll('div').at(1)
-  const msg = '「読み込み中…」.isVisible(): '
 
-  test('読み込み中のときは「読み込み中…」を表示する', done => {
+  test('isUserDataLoading: trueのときは「読み込み中…」を表示する', () => {
     wrapper.setData({ isUserDataLoading: true })
-    expect(wrapper.vm.isUserDataLoading).toBe(true)
 
     wrapper.vm.$nextTick(() => {
-      // console.log(msg + item.isVisible())
+      expect(wrapper.vm.isUserDataLoading).toBe(true)
       expect(item.isVisible()).toBe(true)
-      done()
     })
   })
   
-  test('読み込みをしていないときは「読み込み中…」を表示しない', done => {
+  test('isUserDataLoading: falseのときは「読み込み中…」を表示しない', () => {
     wrapper.setData({ isUserDataLoading: false })
-    expect(wrapper.vm.isUserDataLoading).toBe(false)
 
     wrapper.vm.$nextTick(() => {
-      // console.log(msg + item.isVisible())
+      expect(wrapper.vm.isUserDataLoading).toBe(false)
       expect(item.isVisible()).toBe(false)
-      done()
     })
   })
 })
@@ -42,14 +40,23 @@ describe('Users.vue: ユーザー一覧テーブル', () => {
     },
   ]
   
-  test('usersの数だけ行が表示される', done => {
+  test('usersの数と同じ行数を表示する', () => {
     wrapper.setData({ users: testUsers })
 
     wrapper.vm.$nextTick(() => {
       const item = wrapper.findAll('tbody>tr')
-      // console.log('item: ' + JSON.stringify(item.length))
       expect(item.length).toBe(2)
-      done()
     })
+  })
+})
+
+describe('Users.vue: mount', () => {
+  test('マウント時にgetUsersがコールされる', () => {
+    const spy = jest.spyOn(Api, 'getUsers')
+    expect(spy).toHaveBeenCalledTimes(0)
+
+    const wrapper = shallowMount(Users)
+
+    expect(spy).toHaveBeenCalledTimes(1)
   })
 })
