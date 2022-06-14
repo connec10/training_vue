188701fb1edaeb25e7e285af1fd5487b9736e662
fukuda0910 <105188028+fukuda0910@users.noreply.github.com>
2022-06-14 18:01:41 +0900

    Update UserLogin.test.js

--- training_vue/src/views/__tests__/UserLogin.test.js
+++ training_vue/src/views/__tests__/UserLogin.test.js
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils';
 import UserLogin from '../UserLogin.vue';
+import Auth from 'training_vue/mock/Auth.js'
 
 let wrapper
 
@@ -22,3 +23,25 @@ describe('UserLogin.vue: ログインボタン', () => {
     expect(spy).toBeCalled()
   })
 })
+
+describe('UserLogin.vue: Login関数実行時', () => {
+  test('mock/Auth.jsのlogin関数が呼び出される', () => {
+    const spy = jest.spyOn(Auth, 'login')
+
+    wrapper.vm.$nextTick(() => {
+      wrapper.vm.login()
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
+  });
+
+  test('update-loginイベントがemitされる', () => {
+    const event = wrapper.emitted('update-login')
+    
+    wrapper.vm.login()
+
+    console.log(wrapper.emitted());
+    // wrapper.vm.$nextTick(() => {
+    //   expect(wrapper.emitted().updateLogin).toBeTruthy()
+    // })
+  });
+});
